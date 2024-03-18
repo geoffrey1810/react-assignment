ad563a2eab9d7738801087f73bd8ac1808074421
geoffrey1810 <geoffreymantony@gmail.com>
2024-03-17 22:40:02 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Playlist from './Playlist';
-import App from "./App"
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
